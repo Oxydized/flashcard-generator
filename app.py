48fbc6cdf6e6c8ade8abfaa1cf599cbca2f84744
William Warren <Warren280740@gmail.com>
2026-05-06 19:04:17 -0400

    Refactored file loading into dedicated module

--- app.py
+++ app.py
@@ -1,33 +1,6 @@
 from difflib import SequenceMatcher
-from docx import Document
-from pypdf import PdfReader
+from file_loader import load_file_text
 import csv
-
-def load_file_text(file_name):
-    # Handles TXT file type
-    if file_name.lower().endswith(".txt"):
-        with open(file_name, "r", encoding="utf8") as file:
-            return file.readlines()
-    
-    # Handles DOCX file type
-    elif file_name.lower().endswith(".docx"):
-        document = Document(file_name)
-        return [paragraph.text for paragraph in document.paragraphs]
-    
-    # Handles PDF file type
-    elif file_name.lower().endswith(".pdf"):
-        reader = PdfReader(file_name)
-        lines = []
-
-        for page in reader.pages:
-            text = page.extract_text()
-            if text: 
-                lines.extend(text.splitlines())
-
-        return lines
-        
-    print("Unsupported file type. Please use a .txt or .docx file for now")
-    return None
 
 # Cleans the data before storage
 def clean_term(term):
