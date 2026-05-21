557991ae2212bcfef7aa100a4baf8993d2d4aca5
William Warren <Warren280740@gmail.com>
2026-05-21 01:44:54 -0400

    Allow deployed frontend origin in backend CORS

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=["http://localhost:4200", 
+    "https://flashcard-generator-ochre.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
